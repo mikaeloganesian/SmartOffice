3ad073beaaf5e7945cb22c6c9ffa3835d498fccc
15lu.akari <92751432+mikaeloganesian@users.noreply.github.com>
2025-07-14 06:32:42 +0300

    change title

--- smart-office-frontend/src/pages/LoginPage.jsx
+++ smart-office-frontend/src/pages/LoginPage.jsx
@@ -42,7 +42,7 @@ const LoginPage = () => {
                             type="email"
                             id="email"
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                            placeholder="user@example.com или admin@example.com"
+                            placeholder="example@edu.hse.ru"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                         />
